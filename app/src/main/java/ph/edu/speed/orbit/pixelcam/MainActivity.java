eda5e18a726861daaea1c1cae541d54188ec577d
lexelouse05 <jj_salazar05@hotmail.com>
2015-06-20 10:42:18 +0800

    minor edits

--- app/src/main/java/ph/edu/speed/orbit/pixelcam/MainActivity.java
+++ app/src/main/java/ph/edu/speed/orbit/pixelcam/MainActivity.java
@@ -1,42 +1,38 @@
 package ph.edu.speed.orbit.pixelcam;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.ImageFormat;
-import android.graphics.Rect;
-import android.graphics.YuvImage;
 import android.hardware.Camera;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 
 
 public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback {
 
     Camera camera;
     SurfaceHolder surfaceHolder;
-    Integer pixel,red,green,blue;
+    int pixel, red, green, blue;
     Bitmap bitmap;
+    Boolean isLedOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SurfaceView surfaceview = (SurfaceView) findViewById(R.id.svCam);
+        final SurfaceView surfaceview = (SurfaceView) findViewById(R.id.svCam);
         surfaceHolder = surfaceview.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setFormat(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
     }
+
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
@@ -48,7 +44,7 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
 
             //setting parameters
             Camera.Parameters parameters = camera.getParameters();
-            parameters.setPreviewSize(320, 240);
+            //parameters.setPreviewSize(320, 240);
             parameters.setPreviewFrameRate(30);
             parameters.setSceneMode(Camera.Parameters.SCENE_MODE_NIGHT);
             parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
@@ -60,8 +56,6 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     @Override
@@ -76,26 +70,21 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
 
                 int frameHeight = camera.getParameters().getPreviewSize().height;
                 int frameWidth = camera.getParameters().getPreviewSize().width;
-                int rgb[] = new int[frameWidth * frameHeight]; // number of pixels
+                final int rgb[] = new int[frameWidth * frameHeight]; // number of pixels
 
                 decodeYUV420SP(rgb, data, frameWidth, frameHeight);
-                new pixelcomparator(rgb);
 
-                int dcolor = pixelcomparator.temp;
-                int dcount = pixelcomparator.max;
+                bitmap = Bitmap.createBitmap(frameWidth, frameHeight, Bitmap.Config.RGB_565);
+                bitmap.setPixels(rgb, 0, frameWidth, 0, 0, frameWidth, frameHeight);
+                int dcolor = bitmap.getPixel(frameWidth / 2, frameHeight / 2);
 
-                bitmap  = Bitmap.createBitmap(frameWidth,frameHeight, Bitmap.Config.ARGB_4444);
-                bitmap.setPixels(new int[]{dcolor}, 0, 1, 0, 0,1,1);
-                pixel = bitmap.getPixel(0,0);
-                red = Color.red(pixel);
-                green = Color.green(pixel);
-                blue = Color.blue(pixel);
+                red = Color.red(dcolor);
+                green = Color.green(dcolor);
+                blue = Color.blue(dcolor);
 
                 statView.setText("R: " + String.valueOf(red) + " " +
-                                 "G: " + String.valueOf(green) + " " +
-                                 "B: " + String.valueOf(blue));
-
-
+                        "G: " + String.valueOf(green) + " " +
+                        "B: " + String.valueOf(blue));
 
 
                 SurfaceView surfaceview = (SurfaceView) findViewById(R.id.svCam);
@@ -106,6 +95,7 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
                         result.setText("R: " + String.valueOf(red) + " " +
                                 "G: " + String.valueOf(green) + " " +
                                 "B: " + String.valueOf(blue));
+
                     }
                 });
             }
@@ -129,9 +119,12 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
                         int g = (y1192 - 833 * v - 400 * u);
                         int b = (y1192 + 2066 * u);
 
-                        if (r < 0) r = 0; else if (r > 262143) r = 262143;
-                        if (g < 0) g = 0; else if (g > 262143) g = 262143;
-                        if (b < 0) b = 0; else if (b > 262143) b = 262143;
+                        if (r < 0) r = 0;
+                        else if (r > 262143) r = 262143;
+                        if (g < 0) g = 0;
+                        else if (g > 262143) g = 262143;
+                        if (b < 0) b = 0;
+                        else if (b > 262143) b = 262143;
 
                         rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
                     }
@@ -143,12 +136,18 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
 
     }
 
-
-
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
         camera.release();
+
+    }
+
+    @Override
+    protected void onPause() {
+        camera.unlock();
+        super.onPause();
+
 
     }
 }
